b3ed9b3c800577ae8276fe5e3cfe24b8d18a8d87
4z4q <skodrme@gmail.com>
2025-01-08 01:02:11 +0300

    create header

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,23 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+import Signup from "./pages/SignUp";
+import Profile from "./pages/Profile";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  { path: "/about", element: <About /> },
+  { path: "/signin", element: <Signin /> },
+  { path: "/signup", element: <Signup /> },
+  { path: "/Profile", element: <Profile /> },
+
+]);
+
 export default function App() {
   console.log("hello");
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return <RouterProvider router={router} />;
 }
